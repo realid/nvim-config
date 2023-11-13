427b84ac74a28111a636196918396c7534ae4ac3
LiDan <lidan@superred.com.cn>
2023-11-13 11:00:44 +0800

    修改语法对齐的设置，采用nvim-treesitter的语法对齐

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "bash", "cmake", "html", "css", "vim", "lua", "javascript", "typescript", "c", "cpp", "python"},
+    ensure_installed = { "markdown", "bash", "cmake", "html", "css", "vim", "lua", "javascript", "typescript", "c", "cpp", "python"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    -- sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
-    -- auto_install = true,
+    auto_install = true,
 
     -- List of parsers to ignore installing (for "all")
     -- ignore_install = { "javascript" },
@@ -31,7 +31,7 @@ require'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     },
 
-    -- indent = {
-    --    enable = true
-    -- }
+    indent = {
+       enable = true
+    }
 }
