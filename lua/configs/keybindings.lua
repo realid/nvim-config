1daae90ea9c3cc69a45e7ffde4e58581024e99d6
LiDan <lidan@superred.com.cn>
2024-04-07 16:34:01 +0800

    删除一些快捷键绑定

--- lua/configs/keybindings.lua
+++ lua/configs/keybindings.lua
@@ -8,5 +8,5 @@ local map = vim.api.nvim_set_keymap
 vim.g.mapleader = ','
 
 map("c", "<C-a>", ":CocCommand clangd.switchSourceHeader<CR>", opt)
-map("n", "<C-m>", ":NvimTreeToggle<CR>", opt)
+-- map("n", "<C-m>", ":NvimTreeToggle<CR>", opt)
 map("n", "<C-,>", ":SymbolsOutline<CR>", opt)
